5a1587c23933fce22732424b5343a2fc2766a3ec
pathangenagarjuna <pathangenagarjuna@gmail.com>
2026-08-23 23:51:32 +0530

    Fix CORS for production frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://resumescreenerart.netlify.app",
+        "https://resumescreenermart.netlify.app",
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:3000",
