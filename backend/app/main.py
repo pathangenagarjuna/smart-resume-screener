936296cd4ca39b24443de7cb625abf786cd652bb
pathangenagarjuna <pathangenagarjuna@gmail.com>
2026-08-23 23:32:24 +0530

    Fix CORS for production frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3000",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
